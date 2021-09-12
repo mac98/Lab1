3cbd6244c1911db84804d365444e47ef58af8173
Efrice Antice <macum98@gmail.com>
2021-09-12 12:56:18 -0700

    Add project files.

---
+++ Part1/Program.cs
@@ -0,0 +1,71 @@
+﻿/// Chapter No. 2		Lab No. 1
+/// File Name: Program.cs
+/// @author: Marcus Moore
+/// Date:  September 12, 2021
+/// 
+/// Modifications:
+/// A) Now adds 3 numbers rather than 2.
+/// B) Now also finds the product of the 3 numbers.
+/// C) Finds the quotient of the sum and product.
+///
+/// Problem Statement: Ask the user to enter two numbers, calculate the sum of
+/// these two numbers, and display the sum to the screen
+/// 
+/// Overall Plan:
+/// 1) Print an initial welcoming message to the screen
+/// 2) Prompt the user for two integers
+/// 3) Calculate the sum of the integers
+/// 4) Print the sum of the integers to the screen
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // print a message to the screen
+            Console.WriteLine("Hello out there.");
+            Console.WriteLine("I will add and multiply three numbers for you.");
+            Console.WriteLine("Enter one number and press enter, then do the same for the second and third numbers:");
+
+            // declare 3 integer variables
+            int n1, n2, n3, sum, prod, quot;
+
+            string userInput;
+            userInput = Console.ReadLine();
+            n1 = Int32.Parse(userInput);
+            n2 = Int32.Parse(Console.ReadLine());
+            n3 = Int32.Parse(Console.ReadLine());
+
+            // calculate the sum and product of these two numbers
+            sum = n1 + n2 + n3;
+            prod = n1 * n2 * n3;
+
+            // calculate the quotient of the sum divided by the product
+            quot = sum / prod;
+
+            // print a message along with the sum of the 3 numbers.
+            Console.WriteLine("The sum of the three numbers is");
+            Console.WriteLine(sum);
+
+            //print a message containing the product of the 3 numbers.
+            Console.WriteLine("The product of the three numbers is");
+            Console.WriteLine(prod);
+
+            //print a message containing quotient of the sum and product.
+            Console.WriteLine("The quotient of the sum and product is");
+            Console.WriteLine(quot);
+
+            //Just to pause the screen.
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,46 @@
+﻿/// Chapter No. 2		Lab No. 2
+/// File Name: Program.cs
+/// @author: Marcus Moore
+/// Date:  September 12, 2021
+///
+/// Problem Statement: Given a number of tickets won from an arcade machine and that the price of a candy bar
+/// is 10 tickets and a gumball is 3 tickets, find the maximum number of candy bars and gumballs you can buy
+/// with the entered number of ticket, focusing mainly on candy bars with the leftovers going to gumballs.
+/// 
+/// Overall Plan:
+/// 1) Create an int called tickets.
+/// 2) Prompt user to enter the number of tickets they have won. Save this value into tickets.
+/// 3) Create two ints, candy_bars and gumballs.
+/// 4) Store the number of tickets divided by 10 into candy_bars.
+/// 5) Create a temp variable to store the mod of tickets and 10.
+/// 6) Divide the temp variable by 3 and store this in gumballs.
+/// 7) Print a message stating how many candy bars and gumballs the user can buy.
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program tells you how many candy bars and gumballs you can buy!");
+            int tickets, candy_bars, gumballs, temp;
+            Console.WriteLine("Enter the number of tickets you won:");
+            tickets = Int32.Parse(Console.ReadLine());
+
+            candy_bars = tickets / 10;
+            temp = tickets % 10;
+            gumballs = temp / 3;
+
+            Console.WriteLine("You can buy " + candy_bars + " candy bar(s) and " + gumballs + " gumball(s).");
+
+            Console.ReadLine();
+        }
+    }
+}
